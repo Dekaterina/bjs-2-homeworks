8ba228a413c7b349037e5f21d2c26ccc0414ce4d
Ekaterina Romanova <Dekaterina83@mail.ru>
2025-03-22 12:52:17 +0300

    initial commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,29 @@
 function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  for (let num of arr) {
+    if (num > max)max = num;
+    if(num < min)min = num;
+    sum += num;
+  }
+  const avg = Number((sum / arr.length).toFixed(2));
+  return { min, max, avg };
 }
-
-function summElementsWorker(...arr) {
-
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+    return false;
+  }
+  const percentP = percent / 100 / 12;
+  const creditBody = amount - contribution;
+  const monthlyPayment = creditBody * (percentP + (percentP / (((1 + percentP) ** countMonths) - 1)));
+  const totalAmount = monthlyPayment * countMonths;
+  return Number(totalAmount.toFixed(2));
 }
+console.log(getArrayParams(-99, 99, 10));
+console.log(getArrayParams(1, 2, 3, -100, 10));
+console.log(getArrayParams(5));
 
-function differenceMaxMinWorker(...arr) {
 
-}
 
-function differenceEvenOddWorker(...arr) {
 
-}
-
-function averageEvenElementsWorker(...arr) {
-
-}
-
-function makeWork (arrOfArr, func) {
-
-}
