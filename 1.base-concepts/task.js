c09a5dbc95d7451cca926b01f3764fe6c565a1f7
Ekaterina Romanova <Dekaterina83@mail.ru>
2025-03-22 08:24:11 +0300

    initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,18 @@
 "use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+function solveEquation(a, b, c) {
+  const discriminant = b ** 2 - 4 * a * c;
   
+  if (discriminant < 0) {
+    return [];
+  }
+  
+  if (discriminant === 0) {
+    const root = -b / (2 * a);
+    return [root];
+  }
+  
+  const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+  const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+  return [root1, root2];
 }
