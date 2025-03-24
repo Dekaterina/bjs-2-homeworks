95aea41a3bade0f359db8f704da12a5edfd9c51a
Ekaterina Romanova <Dekaterina83@mail.ru>
2025-03-24 08:08:45 +0300

    initial commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,40 @@
+"use strict";
+
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  return arr1.every((element, index) => element === arr2[index]);
 }
 
+
+console.log(compareArrays([8,9], [6])); 
+console.log(compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5])); 
+console.log(compareArrays([8, 1, 2], [8, 1, 2]));
+
+
+
+
+"use strict";
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+  if (!users.length) {
+    return 0;
+  }
+
+  const filteredUsers = users
+    .filter(user => user.gender === gender)
+    .map(user => user.age)
+    .reduce((acc, age, index, array) => {
+      acc += age;
+      if (index === array.length - 1) {
+        return acc / array.length;
+      }
+      return acc;
+    }, 0);
+
+  return filteredUsers || 0;
 }
+
+
