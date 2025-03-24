00e6ed0b39102e03fcaf53efda3e034c18a1ae1e
Ekaterina Romanova <Dekaterina83@mail.ru>
2025-03-24 14:06:57 +0300

    initial commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
+"use strict";
+
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
+};
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+Student.prototype.addMarks = function(...marksToAdd) {
+  if (!this.marks) {
+    return;
+  }
+  this.marks.push(...marksToAdd);
+};
 
-Student.prototype.getAverage = function () {
-  
-}
+Student.prototype.getAverage = function() {
+  if (!this.marks || !this.marks.length) {
+    return 0;
+  }
+  return this.marks.reduce((sum, mark) => sum + mark, 0) / this.marks.length;
+};
 
-Student.prototype.exclude = function (reason) {
-  
-}
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+};
+
+
+
